b09984a277520c3d44a4cd07a29c95082354d80f
jorgeucano <jorgeucano@gmail.com>
2016-04-28 08:16:33 -0300

    esto se va a descontrolaaar

--- app.jsx
+++ app.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
 import reactDOM from 'react-dom';
-import Hello from './message';
+import Message from './message';
+import Link from './link';
 
-reactDOM.render(<Hello name='React'/>, document.getElementById('container'));
+class App extends React.Component{
+  render(){
+    const url = 'https://github.com/jorgeucano/CursoReactV15/';
+
+    return(
+      <div>
+        <Message />
+        <nav>
+          <Link url={url} caption="My github" />
+          <Link url='https://twitter.com/jorgeucano' caption="My Twitter" />
+        </nav>
+      </div>
+    );
+  }
+}
+
+reactDOM.render(<App />, document.getElementById('container'));
---
+++ link.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class Link extends React.Component{
+  render(){
+    return <a href={this.props.url}>{this.props.caption}</a>
+  }
+}
+export default Link;
